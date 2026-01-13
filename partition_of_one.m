079a9380553ef3c46ac704b415e676e4fb061e8b
Torge Graner <tgraner@uni-osnabrueck.de>
2026-01-13 14:35:49 +0100

    Commented

--- partition_of_one.m
+++ partition_of_one.m
@@ -1,15 +1,22 @@
+% Render the manifold defined by the equation cos(x) + sin(y) - z^2 = 1 by scaling points on the unit sphere using Newtons method
+% and color it according to a partition of one
+
 g = @(t) f(t) ./ (f(t) + f(1 - t));
 a = @(x, y) 1 - g(sqrt(x.^2 + y.^2) - 1);
 k = 4;
 
+
+% Parameter space
 [theta, phi] = meshgrid(0:pi/600:pi, 0:pi/400:2*pi);
 
+% Standard parameterization of sphere
 X = sin(theta) .* cos(phi);
 Y = sin(theta) .* sin(phi);
 Z = cos(theta);
 
 X_n = pi/4 * X ./ X;
 
+% Apply a step of Newtons method to the function f(r) = cos(rx) + sin(ry) - (rz)^2 - 1 to approximate a scalar r, such that r*(x,y,z) lies in the manifold
 for i = 1:20
     ff = cos(X_n .* X) + cos(X_n .* Y) - (X_n .* Z).^2 - 1;
     fp = X .* sin(X_n .* X) + Y .* sin(X_n .* Y) + 2 * Z.^2 .* X_n;
@@ -35,7 +42,7 @@ hold on
     %exportgraphics(gca, 'PartitionOfOne.eps');
 hold off
 
-
+% Color the point on the sphere corresponding to the overlap of a partition of one 
 function r = colorMap(X, Y, a_1, a_2, a, k)
     F_x = X * a_1;
     F_y = Y * a_2;
